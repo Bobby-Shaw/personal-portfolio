71bbc62d0e9f86603ea3122ced89b70e097e4bec
bobbyshaw <shawuk2006@gmail.com>
2026-01-10 19:15:40 +0000

    Added a contact page and made other design edits

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+function sendEmail() {
+    let email = document.getElementById("email-input").value;
+    let name = document.getElementById('name-input').value;
+    let message = document.getElementById('message-input').value;
+}
