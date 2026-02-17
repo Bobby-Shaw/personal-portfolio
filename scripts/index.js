986eba01ff811b73cfb43c2ce9fc298c99f6fc51
Bobby Shaw <shawuk2006@gmail.com>
2026-02-17 17:06:22 +0000

    minor changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,9 @@ function sendEmail() {
     let name = document.getElementById('name-input').value;
     let message = document.getElementById('message-input').value;
 }
+
+function changeVisibility() {
+    console.log("Hello World");
+    let navBar = document.querySelector(".responsive-navbar");
+    navBar.classList.toggle("visible")
+}
